9e5c761b93e439b79449345e14500e9f92f1c637
Aliha Ehsan <alihaehsann@gmail.com>
2026-06-21 12:19:43 +0500

    Fix broken test

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@ def setup_function():
 
 def test_health():
     response = client.get("/health")
-    assert response.status_code == 500
+    assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
 
